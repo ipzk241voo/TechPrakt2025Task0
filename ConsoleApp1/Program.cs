deb018fdfa1022554c754b95071a0991fce071c8
nightpower <alexeyvisnevskiy@gmail.com>
2025-02-01 23:56:30 +0200

    add console app summ

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+WriteLine("Hello, World!");
+
+int[] numbers = new int[2];
+
+for (int i = 0; i < 2; i++)
+{
+    WriteLine("Введіть число:");
+    numbers[i] = int.Parse(ReadLine());
+}
+
+WriteLine($"Сумма: {numbers.Sum()}");
